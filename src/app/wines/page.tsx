21b032e4a34a9831acbb3057d9d1e3a4aa450413
panigrah <sp@sps-mac-mini.lan>
2023-09-26 21:53:01 +0800

    layout changes

--- src/app/wines/page.tsx
+++ src/app/wines/page.tsx
@@ -22,6 +22,7 @@ export default function WineTrackerPage() {
     setQ(e.target.value)
   }
 
+  console.log(results.data?.length, results.isFetching, q)
   return(
     <Page>
       <BlockTitle>Feel lucky?</BlockTitle>
@@ -33,53 +34,50 @@ export default function WineTrackerPage() {
           value={q}
         />
       </List> 
-      <BlockTitle>
-        Results
-      </BlockTitle>
-      <List outline>
-        {results.isFetching && 
-          <ListItem>
-            <div className='flex flex-auto'>
-              <Preloader className='mx-auto' />
-            </div>
-          </ListItem>
-        }
-        {(results.data?.length === 0 && !results.isFetching && q) &&
-          <ListItem
-            title='Nothing found'
-          />
-        }
-         {(results.data?.length === 0 && !results.isFetching && !q) &&
-          <ListItem
-            title='Enter something into the searchbox'
-          />
-        }
-        {results.data?.map( (r:any) => {
-          return <ListItem 
-            key={r.id} 
-            href={r.url}
-            target='new'
-            title={r.src}
-            after={r.sale_price}
-            subtitle={<>
-              <span>{r.name}</span>
-              {r.vintage && <span>-[{r.vintage}]</span>}
-              </>
-            }
-            text={r.summary || r.detail || 'no details provided' }
-            media={
-              r.image?.length ?
-              <img 
-                className="ios:rounded-lg material:rounded-full ios:w-20 aspect-square object-contain material:w-10"
-                src={r.image?.[0]}
-                width="80"
-                alt={r.name}
+      { results.isFetching && <div className='flex flex-auto h-full'><Preloader className='mx-auto mt-20' /></div> }
+      { (!results.isFetching && !results.data?.length) && (
+          q?
+            <Block inset>
+              No results found
+            </Block>
+          :
+            <Block inset>
+              Enter something into the search box
+            </Block>
+        )
+      }
+      {results.data?.length &&
+        <>
+          <BlockTitle>Results</BlockTitle>
+          <List outline>
+            {results.data?.map( (r:any) => {
+              return <ListItem 
+                key={r.id} 
+                href={r.url}
+                target='new'
+                title={r.src}
+                after={r.sale_price}
+                subtitle={<>
+                  <span>{r.name}</span>
+                  {r.vintage && <span>-[{r.vintage}]</span>}
+                  </>
+                }
+                text={r.summary || r.detail || 'no details provided' }
+                media={
+                  r.image?.length ?
+                  <img 
+                    className="ios:rounded-lg material:rounded-full ios:w-20 aspect-square object-contain material:w-10"
+                    src={r.image?.[0]}
+                    width="80"
+                    alt={r.name}
+                  />
+                  :undefined
+                }
               />
-              :undefined
-            }
-          />
-        })}
-      </List>
+            })}
+          </List>
+        </>
+      }
     </Page>
   )
 }
